fa7f194a40be9dd136a56aae0698e8b02a2bbcce
Taras <tarasostapchyk27@gmail.com>
2021-05-20 23:37:46 +0300

    Login Auth

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import {Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = (props) => {
     return (
         <div className="app-wrapper">
-            <Header/>
+            <HeaderContainer/>
             <Navbar/>
             <div className="app-wrapper-content">
                 <Route path='/profile/:userId?' render={ () => <ProfileContainer />}/>
