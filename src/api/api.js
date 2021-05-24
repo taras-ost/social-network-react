68bbe19e5bbab905366b67d8d44216ceb4e6686b
Taras <tarasostapchyk27@gmail.com>
2021-05-24 23:53:30 +0300

    Axios.Create and API

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {'API-KEY': 'b847ec71-0d31-411f-8a16-ec8f56ee2501'}
+})
+
+export const usersAPI = {
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            })
+    },
+    followUser() {
+
+    }
+}
